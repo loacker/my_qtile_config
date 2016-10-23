7ed2f7c995cb9f8c4ea0500eac2da75196f32d1e
matteo [l0aCk3r] <loacker@autistici.org>
2016-10-23 17:30:26 +0200

    Change DF

--- autostart.sh
+++ autostart.sh
@@ -5,7 +5,7 @@ xmodmap ${HOME}/.Xmodmap
 
 #${HOME}/bin/runonce "$(which xscreensaver) -no-splash"
 
-/usr/bin/urxvt -e screen -R -D &
+/usr/bin/urxvt -e screen -R -D -S local &
 /usr/bin/urxvt -e weechat-curses &
 /usr/bin/firefox &
 /usr/bin/chromium-browser -disable-prompt-on-repost &
